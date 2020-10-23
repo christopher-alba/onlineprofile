61a6ac8e797c10622651e1feaf0e4fd99424c2f5
albasychris <albasychris@gmail.com>
2020-10-23 21:30:44 +1300

    implement routes to portfolio and home

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Home from './Home'
 import Navbar from './Navbar'
+import Portfolio from './Portfolio'
+import { Route } from 'react-router-dom'
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <Home />
+      <Navbar />
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path='/portfolio'>
+        <Portfolio />
+      </Route>
+
     </>
   )
 }
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
 import './navbar.css'
+import { Link } from 'react-router-dom'
 class MainNavbar extends Component {
+  state = {
+    location: ''
+  }
   render() {
     return (
-      <Container className="mainNavbar" style={{padding:'0px'}}>
+      <Container className="mainNavbar" style={{ padding: '0px' }}>
         <Navbar bg="light" expand="lg" >
           <Navbar.Brand href="#home">Christopher Alba</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Portfolio</Nav.Link>
-              <Nav.Link href="#link">Blog</Nav.Link>
-              <Nav.Link href="#link">Contact</Nav.Link>
+              <Link to="/" ><Nav.Link href="#home" >Home</Nav.Link></Link>
+              <Link to="/portfolio"><Nav.Link href="#link" >Portfolio</Nav.Link></Link>
+              <Link to="/blog"><Nav.Link href="#link" >Blog</Nav.Link></Link>
+              <Link to="/contact"><Nav.Link href="#link">Contact</Nav.Link></Link>
+           
             </Nav>
           </Navbar.Collapse>
         </Navbar>
