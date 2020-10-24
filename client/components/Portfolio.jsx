37e3b39b1080cb84db2def324c22bf45fb2f8273
albasychris <albasychris@gmail.com>
2020-10-24 21:48:03 +1300

    set up portfolio

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import './portfolio.css'
+import projects from '../projects'
+import Button from 'react-bootstrap/Button'
+
 class Portfolio extends Component {
   render() {
     return (
       <div className="portfolioContainer ">
         <div className="portfolioHeader">
           <h3>Welcome to my Portfolio</h3>
-          <img src="https://www.fictiontalk.com/wp-content/uploads/2020/08/essential-things-you-need-for-a-perfect-gaming-setup.jpg" alt=""/>
+          <img src="https://www.fictiontalk.com/wp-content/uploads/2020/08/essential-things-you-need-for-a-perfect-gaming-setup.jpg" alt="" />
+        </div>
+        <div className="portfolioInner">
+          {projects.map(project => (
+            <div className="projectContainer">
+              <img src={project.imgUrl} alt="" />
+              <Button className="visit" href={project.href} variant="dark"><i className="fas fa-external-link-alt"></i></Button>
+              <h4>{project.title}</h4>
+              <p>{project.description}</p>
+              <div className="hashtags">
+                {project.tags.map(tag => <div className="hashtag"><strong>#{tag}</strong></div>)}
+              </div>
+
+            </div>
+          ))}
         </div>
 
       </div>
---
+++ client/projects.js
@@ -0,0 +1,159 @@
+export default [
+  {
+    title: '2D Space Shooter',
+    imgUrl: './images/galaga.png',
+    description:
+'I made this classic style shooter during the foundations section of the Enspiral Dev Academy bootcamp and was not part of the curiculum. Play until you die as there are infinite rounds and enemies. HAVE FUN!',
+    href: `https://christopher-alba.github.io/galaga-clone/`,
+    tags: ['HTML', 'CSS', 'Javascript', 'jQuery', 'Desktop', 'Game']
+  },
+  {
+    title: 'Basic Scientific Calculator',
+    imgUrl: './images/calculator.png',
+    description:
+'This project is an extension of an exercise during foundations at EDA. It has basic arithmetic operations, stored temporary memory and other basic functions of a scientific calculator. It also has an ON/OFF button.',
+    href: `https://christopher-alba.github.io/calculator/`,
+    tags: ['HTML', 'CSS', 'Javascript', 'jQuery', 'Desktop', 'Mobile']
+  },
+  {
+    title: 'PvP Chess with Assists',
+    imgUrl: './images/chess.png',
+    description:
+'I made this during the last week of foundations at EDA and was not part of the curiculum. It features chess with all the computer assists that prevent any illegal moves based on the original rules of classic chess. Unfortunately, there is no AI (yet).',
+    href: `https://christopher-alba.github.io/chess/`,
+    tags: ['HTML', 'CSS', 'Javascript', 'jQuery', 'Desktop', 'Game']
+  },
+  {
+    title: 'Dress the Clown',
+    imgUrl: './images/clown.png',
+    description:
+'This was an extension of an exercise during sprint 5 of foundations at EDA. It features the ability to change the attire of the clown and save it as an image. Have fun mixing and matching!',
+    href: `https://christopher-alba.github.io/javascript-carnival/dress-the-clown/dress-the-clown.html`,
+    tags: ['HTML', 'CSS', 'Javascript', 'Desktop', 'Mobile', 'Game']
+  },
+  {
+    title: 'Whack-a-Mole',
+    imgUrl: './images/mole.png',
+    description:
+'This was an extension of an exercise during sprint 5 of foundations at EDA. It is similar to one of those games at a carnival where you smack things that pop out. Have fun clicking moles!',
+    href: `https://christopher-alba.github.io/javascript-carnival/whack-a-mole/whack-a-mole.html`,
+    tags: ['HTML', 'CSS', 'Javascript', 'Desktop', 'Mobile', 'Game']
+  },
+  {
+    title: 'EDA Foundations Personal Blog',
+    imgUrl: './images/blog.png',
+    description:
+'I created this blog as part of the EDA foundations curiculum and it contains my reflections on my learning and on myself. If you want to know more about my experience during foundations or you want to know more about me, feel free to visit my blog.',
+    href: `https://christopher-alba.github.io/`,
+    tags: [
+      'HTML',
+      'CSS',
+      'Bootstrap',
+      'Javascript',
+      'jQuery',
+      'Desktop',
+      'Mobile',
+      'Blog'
+    ]
+  },
+  {
+    title: 'To do List with Persisting Memory',
+    imgUrl: './images/list.png',
+    description: `I made this to do list when I was learning from an online bootcamp from Udemy. It features the ability to add and delete lists, as well as ability to add and delete items within a list. Feel free to leave a nice message if you visit ❤.`,
+    href: `https://cryptic-shore-73893.herokuapp.com/`,
+    tags: [
+      'HTML',
+      'CSS',
+      'Javascript',
+      'jQuery',
+      'EJS',
+      'Express',
+      'MongoDB',
+      'Mongoose',
+      'Mobile',
+      'Desktop'
+    ]
+  },
+  {
+    title: 'Small Scale E-Commerce Site',
+    imgUrl: './images/janzart.png',
+    description: `This project began during week 5 of the EDA bootcamp for my personal project presentation in week 7. It is a simulation of an e-commerce website and all the data inside the postgreSQL database is just seed data.The payment system is running in test mode so no real payments can be processed, only tested. Enjoy your visit ❤.`,
+    href: `https://janzart.herokuapp.com/#/`,
+    tags: [
+      'HTML',
+      'CSS',
+      'Javascript',
+      'Node.js',
+      'Express.js',
+      'Knex.js',
+      'React.js',
+      'React-Redux',
+      'Semantic-UI-React',
+      'Auth0',
+      'Stripe',
+      'Other React Libraries',
+      'Desktop',
+      'Mobile',
+      'eCommerce'
+    ]
+  },
+  {
+    title: 'Tourism Site',
+    imgUrl:'./images/tourismNZ.png',
+    description: 'This project is an extension of the project AoteaWoah that I did during the Summer of Tech Create Camp 2020. It is designed to help boost local tourism in NZ and uses a weather API aswell as the google maps API.' ,
+    href: 'https://christopher-alba.github.io/AdventureNZ/?#/' ,
+    tags: [
+      'HTML',
+      'CSS',
+      'Javascript',
+      'React.js',
+      'React-Redux',
+      'Semantic-UI-React',
+      'Desktop',
+      'Mobile',
+      'Weather API',
+      'Google Maps API'
+    ]
+
+
+  },
+  {
+    title: 'Binary Options Manager',
+    imgUrl: './images/financialPlanner.png',
+    description: 'An app that helps users manage their binary options trading by manually inputing trades and calculating how many successful trades they need to do at minimum.',
+    href: 'https://binaryoptions.herokuapp.com/#/',
+    tags: [
+      'HTML',
+      'CSS',
+      'Javascript',
+      'React.js',
+      'React-Redux',
+      'React-Bootstrap',
+      'Desktop',
+      'Passport.js',
+      'Node.js',
+      'Express.js',
+      'Knex.js'
+    ]
+  },
+  {
+    title: 'PennyWise',
+    imgUrl: '/images/pennywise.png',
+    description: 'This is a money management app that is designed to allow users to plan well ahead into the future to achieve their financial goals. This was my final project in the EDA bootcamp, which I built along side 5 other team members. We built this project in an agile environment within a week and used Github projects to manage our workflow.',
+    href: 'http://pennywise-eda.herokuapp.com/#/',
+    tags: [
+      'HTML',
+      'CSS',
+      'Javascript',
+      'React.js',
+      'Redux',
+      'Node.js',
+      'Express.js',
+      'Knex.js',
+      'Passport.js',
+      'Semantic-UI-React',
+      'Other React Libraries',
+      'Desktop'
+    ]
+  }
+]
