8409d43979337de0502a61705dcf6c1595ec6d70
Christopher <calb200@aucklanduni.ac.nz>
2020-12-05 14:02:29 +1300

    improve hashtag searching

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -11,17 +11,33 @@ class Search extends Component {
 
     handleChange = (evt) => {
 
+        let strings = evt.target.value.split(" ")
+
+        for(let i = 0; i < strings.length; i++){
+            if(strings[i] === ""){
+                strings.splice(i,1)
+                i--
+            }
+        }
+        console.log(strings);
         if (this.state.filter === 'title') {
             this.setState({ projects: projects.filter(project => project.title.toLowerCase().includes(evt.target.value.toLowerCase())) })
         } else {
             this.setState({
                 projects: projects.filter(project => {
-
-                    for (let i = 0; i < project.tags.length; i++) {
-                        if (project.tags[i].toLowerCase().includes(evt.target.value.toLowerCase())) {
-                            return true
+                    let counter = 0
+                    for (let j = 0; j < strings.length; j++) {
+                        for (let i = 0; i < project.tags.length; i++) {
+                            if (project.tags[i].toLowerCase().includes(strings[j].toLowerCase())) {
+                                counter ++
+                            }
                         }
                     }
+
+                    if(counter === strings.length){
+                        return true
+                    }
+
                     return false
 
                 })
@@ -34,9 +50,25 @@ class Search extends Component {
         return (
             <>
                 <div className="searchBar">
-                    <div className={`${this.state.filter === 'title' ? 'selected ' : ''} button`} onClick={() => this.setState({ filter: 'title' }, () => console.log(this.state.filter))}>title</div>
-                    <div className={`${this.state.filter === 'hashtags' ? 'selected ' : ''} button`} onClick={() => this.setState({ filter: 'hashtags' }, () => console.log(this.state.filter))}>hashtags</div>
-                    <input className="input" type="text" placeholder={this.state.filter === 'title' ? "search for project title": "search for project hashtag"} onChange={this.handleChange} />
+                    <div className={`${this.state.filter === 'title' ? 'selected ' : ''} button`} onClick={() => {
+                        
+                        this.setState({ filter: 'title' })
+                        document.getElementsByClassName("input")[0].value = ''
+                        this.handleChange({target:{
+                            value: ''
+                        }})
+
+                    }}>title</div>
+                    <div className={`${this.state.filter === 'hashtags' ? 'selected ' : ''} button`} onClick={() => {
+                        
+                        this.setState({ filter: 'hashtags' })
+                        document.getElementsByClassName("input")[0].value = ''
+                        this.handleChange({target:{
+                            value: ''
+                        }})
+                        
+                    }}>hashtags</div>
+                    <input className="input" type="text" placeholder={this.state.filter === 'title' ? "search for project title" : "search for project hashtag"} onChange={this.handleChange} />
                 </div>
 
                 <Projects projects={this.state.projects} />
